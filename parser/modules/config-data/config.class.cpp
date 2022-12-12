94bcf53a938171c9b57a62116d5fdef6e4a3d832
Anas Qadil <qadilanass10@gmail.com>
2022-12-12 20:39:38 +0100

    implement config class

--- parser/modules/config-data/config.class.cpp
+++ parser/modules/config-data/config.class.cpp
@@ -1,3 +1,88 @@
 #include <iostream>
+#include "./config.class.hpp"
 
-using namespace std;
+Config::Config() {
+	// fill with default values
+	this->location = Location();
+	this->port.push_back(80);
+	this->server_name.push_back("localhost");
+	this->host = "localhost";
+	this->client_max_body_size = 1;
+	this->root = "/";
+}
+// copy constructor
+Config::Config(const Config &config) {
+	this->location = config.location;
+	this->port = config.port;
+	this->server_name = config.server_name;
+	this->host = config.host;
+	this->client_max_body_size = config.client_max_body_size;
+	this->root = config.root;
+}
+
+// getters
+Location Config::getLocation() const {
+	return this->location;
+}
+std::vector<int> Config::getPort() const {
+	return this->port;
+}
+std::vector<std::string> Config::getServerName() const {
+	return this->server_name;
+}
+std::string Config::getHost() const {
+	return this->host;
+}
+int Config::getClientMaxBodySize() const {
+	return this->client_max_body_size;
+}
+std::string Config::getRoot() const {
+	return this->root;
+}
+
+// setters
+void Config::setLocation(Location location) {
+	this->location = location;
+}
+void Config::setPort(std::vector<int> port) {
+	this->port = port;
+}
+void Config::setServerName(std::vector<std::string> server_name) {
+	this->server_name = server_name;
+}
+void Config::setHost(std::string host) {
+	this->host = host;
+}
+void Config::setClientMaxBodySize(int client_max_body_size) {
+	this->client_max_body_size = client_max_body_size;
+}
+void Config::setRoot(std::string root) {
+	this->root = root;
+}
+
+// other methods
+void Config::printConfig() const {
+	std::cout << "Config:" << std::endl;
+	// when location is implemented, uncomment this
+	// std::cout << "location: " << this->location << std::endl;
+	// loop through port vector
+	std::cout << "port: ";
+	for (std::vector<int>::const_iterator it = this->port.begin(); it != this->port.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	// loop through server_name vector
+	std::cout << "server_name: ";
+	for (std::vector<std::string>::const_iterator it = this->server_name.begin(); it != this->server_name.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "host: " << this->host << std::endl;
+	std::cout << "client_max_body_size: " << this->client_max_body_size << std::endl;
+	std::cout << "root: " << this->root << std::endl;
+}
+
+
+Config::~Config() {
+	std::cout << "Config destructor called" << std::endl;
+}
--- parser/modules/config-data/config.class.hpp
+++ parser/modules/config-data/config.class.hpp
@@ -11,6 +11,26 @@ class Config {
 		int	client_max_body_size;
 		std::string root;
 	public:
-		// Config(); // default constructor
-		// ~Config(); // destructor
+		Config(); // default constructor
+		~Config(); // destructor
+		Config(const Config &config); // copy constructor
+
+		// getters
+		Location getLocation() const;
+		std::vector<int> getPort() const;
+		std::vector<std::string> getServerName() const;
+		std::string getHost() const;
+		int getClientMaxBodySize() const;
+		std::string getRoot() const;
+		
+		// setters
+		void setLocation(Location location);
+		void setPort(std::vector<int> port);
+		void setServerName(std::vector<std::string> server_name);
+		void setHost(std::string host);
+		void setClientMaxBodySize(int client_max_body_size);
+		void setRoot(std::string root);
+
+		// other methods
+		void printConfig() const;
 };
